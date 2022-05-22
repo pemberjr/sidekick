f37e2b1d7ac4d6a6108ad88ca0684ca1d7efca8a
Jim Pemberton <75695122+pemberjr@users.noreply.github.com>
2022-05-22 11:46:38 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 # Used to deploy app via docker container
 #
 
@@ -6,7 +5,7 @@ echo -e "Starting Docker deployment script...\n"
 
 #check if container currently running
 
-containerID=$(docker ps | grep netmri-discovery-app | tr -s ' ' | cut -d ' ' -f 1)
+containerID=$(docker ps -a| grep sidekick-app | tr -s ' ' | cut -d ' ' -f 1)
 
 echo -e "$containerID\n"
 if [ -z "$containerID" ]
@@ -15,13 +14,13 @@ then
 else
       echo "\$containerID is NOT NULL"
       # Stop the currently running docker image
-      docker stop netmri-discovery-app
+      docker stop sidekick-app
       # Remove all exited docker images
-      docker rm netmri-discovery-app
+      docker rm sidekick-app
 fi
 
 # Rebuild and re-run docker images
-docker build -t netmri-discovery-app .
-docker run -dp 5050:5050 --name netmri-discovery-app -v /apps/NETMRI_Discovery:/NETMRI_Discovery netmri-discovery-app
+docker build -t sidekick-app .
+docker run -dp 5051:5051 --name sidekick-app --network airflow-tier -v /apps/sidekick:/sidekick sidekick-app
 
 echo -e "\nFinished deploying docker container"
